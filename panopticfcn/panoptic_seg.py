95692f8df0558fda9f04e5132f0fb1d3be0de2a8
sky_konkuk <110399785+Sunggggggg@users.noreply.github.com>
2023-03-12 12:36:04 +0900

    Update panoptic_seg.py

--- panopticfcn/panoptic_seg.py
+++ panopticfcn/panoptic_seg.py
@@ -55,7 +55,17 @@ class PanopticFCN(nn.Module):
         self.thing_generator       = build_thing_generator(cfg)
         self.stuff_generator       = build_stuff_generator(cfg)
         self.get_ground_truth      = GenerateGT(cfg)
-
+        
+        
+        print(self.backbone)
+        print(self.semantic_fpn)
+        print(self.position_head)
+        print(self.kernel_head)
+        print(self.feature_encoder)
+        print(self.thing_generator)
+        print(self.stuff_generator)
+        print(self.get_ground_truth)
+        
         pixel_mean = torch.Tensor(cfg.MODEL.PIXEL_MEAN).to(self.device).view(3, 1, 1)
         pixel_std = torch.Tensor(cfg.MODEL.PIXEL_STD).to(self.device).view(3, 1, 1)
         self.normalizer = lambda x: (x - pixel_mean) / pixel_std
